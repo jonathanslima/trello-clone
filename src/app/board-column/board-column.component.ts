5fd9ec6bbb8fb22f966be459b075878c6ad46943
Lima <jonathan.da.s.lima@accenture.com>
2020-02-23 15:16:57 -0300

    init cols

---
+++ src/app/board-column/board-column.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board-column',
+  templateUrl: './board-column.component.html',
+  styleUrls: ['./board-column.component.scss']
+})
+export class BoardColumnComponent implements OnInit {
+
+  constructor() { }
+
+  public toogleSubmenu(){
+    console.log('toogle')
+    
+  }
+
+  ngOnInit() {
+  }
+
+}
