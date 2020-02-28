662a044cf42129af24b727a7d1761247721c7674
Lima <jonathan.da.s.lima@accenture.com>
2020-02-28 18:12:50 -0300

    iniciando cadastro de users

--- src/app/filtros/filtros.component.ts
+++ src/app/filtros/filtros.component.ts
@@ -12,7 +12,43 @@ export class FiltrosComponent implements OnInit {
   public tags: Array<string>;
   public people: Array<object>;
   public userImg: string;
+  public bol: boolean = true;
+
   constructor(private retrieveData : retrieveData ) { }
+
+  public liberaCadastro(){
+    document.querySelector('.line-1').classList.remove('anim-sub','anim-pls');
+
+    if(this.bol){
+      document.querySelector('.line-1').classList.add('anim-sub');
+      document.querySelector('.step1').classList.add('d-block');
+      document.querySelector('.stp1').classList.add('d-block');
+
+      this.bol = false;
+
+    }else{
+      document.querySelector('.line-1').classList.add('anim-pls');
+      document.querySelector('.step1').classList.remove('d-block');
+      document.querySelector('.stp1').classList.remove('d-block');
+      document.querySelector('.step2').classList.remove('d-block');
+      document.querySelector('.stp2').classList.remove('d-block');
+
+      this.bol = true;
+    }
+  }
+
+  public avanca(){
+    document.querySelector('.step1').classList.remove('d-block');
+    document.querySelector('.stp1').classList.remove('d-block');
+    document.querySelector('.step2').classList.add('d-block');
+    document.querySelector('.stp2').classList.add('d-block');
+  }
+
+  public cadastra(){
+    document.querySelector('.step2').classList.remove('d-block');
+    document.querySelector('.stp2').classList.remove('d-block');
+    console.log('cadastrado usuario')
+  }
 
   ngOnInit() {
     this.retrieveData.getTags()
