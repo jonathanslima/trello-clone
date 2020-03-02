61c4fa049ab356659add2572b00760f7b984ae97
Lima <jonathan.da.s.lima@accenture.com>
2020-03-02 14:46:16 -0300

    add route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,17 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { DragDropModule } from "@angular/cdk/drag-drop";
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from "@angular/forms";
 
+import { AppRoutingModule } from "./app-routing-module";
 import { AppComponent } from "./app.component";
 import { TopoComponent } from "./topo/topo.component";
 import { BuscaFiltrosComponent } from "./busca-filtros/busca-filtros.component";
 import { BuscaComponent } from "./busca/busca.component";
 import { FiltrosComponent } from "./filtros/filtros.component";
 import { BoardColumnComponent } from "./board-column/board-column.component";
-import { CriarColunaComponent } from './criar-coluna/criar-coluna.component';
-import { CriarTaskComponent } from './criar-task/criar-task.component';
+import { CriarColunaComponent } from "./criar-coluna/criar-coluna.component";
+import { CriarTaskComponent } from "./criar-task/criar-task.component";
 
 @NgModule({
   declarations: [
@@ -24,7 +25,13 @@ import { CriarTaskComponent } from './criar-task/criar-task.component';
     CriarColunaComponent,
     CriarTaskComponent
   ],
-  imports: [BrowserModule, HttpClientModule, DragDropModule, FormsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    DragDropModule,
+    FormsModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
