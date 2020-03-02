07b559c27b050f01acfc4d5c35462d75b7578751
Lima <jonathan.da.s.lima@accenture.com>
2020-03-02 15:14:26 -0300

    change url to https

--- src/app/servicos/data.service.ts
+++ src/app/servicos/data.service.ts
@@ -9,53 +9,53 @@ export class retrieveData{
 
   // Title
   public getTitle():Promise<any>{
-    return this.http.get('http://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/titleBoard')
+    return this.http.get('https://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/titleBoard')
       .toPromise()
       .then((title: string) => title)
   }
 
   public updateTitle(data):Promise<any>{
-    return this.http.put('http://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/titleBoard', data)
+    return this.http.put('https://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/titleBoard', data)
       .toPromise()
       .then((res: any) => res)
   }
 
   // Columns
   public getColumns():Promise<any>{
-    return this.http.get('http://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/columns')
+    return this.http.get('https://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/columns')
       .toPromise()
       .then((columns: any) => columns)
   }
 
   public addColumns(data):Promise<any>{
-    return this.http.post('http://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/columns', data)
+    return this.http.post('https://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/columns', data)
       .toPromise()
       .then((columns: any)=> columns)
   }
 
   // Tasks
   public addTask(data, id):Promise<any>{
-    return this.http.put(`http://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/columns/${id}`, data)
+    return this.http.put(`https://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/columns/${id}`, data)
       .toPromise()
       .then((cards: any)=> cards)
   }
 
   // Tags
   public getTags(){
-    return this.http.get('http://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/tags')
+    return this.http.get('https://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/tags')
     .toPromise()
     .then((tags: any) => tags)
   }
 
   // People
   public getPeople(){
-    return this.http.get('http://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/people')
+    return this.http.get('https://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/people')
       .toPromise()
       .then((people:any) => people)
   }
 
   public addUser(data){
-    return this.http.post('http://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/people', data)
+    return this.http.post('https://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/people', data)
       .toPromise()
       .then((user:any)=> user)
   }
