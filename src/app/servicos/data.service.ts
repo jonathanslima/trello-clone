aa844e100295d5d5b0746c5e6a9393eaf75aa821
Lima <jonathan.da.s.lima@accenture.com>
2020-03-03 12:50:48 -0300

    add new endpoint

--- src/app/servicos/data.service.ts
+++ src/app/servicos/data.service.ts
@@ -9,53 +9,53 @@ export class retrieveData{
 
   // Title
   public getTitle():Promise<any>{
-    return this.http.get('https://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/titleBoard')
+    return this.http.get('https://trello-server-xp.herokuapp.com/titleBoard')
       .toPromise()
       .then((title: string) => title)
   }
 
   public updateTitle(data):Promise<any>{
-    return this.http.put('https://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/titleBoard', data)
+    return this.http.put('https://trello-server-xp.herokuapp.com/titleBoard', data)
       .toPromise()
       .then((res: any) => res)
   }
 
   // Columns
   public getColumns():Promise<any>{
-    return this.http.get('https://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/columns')
+    return this.http.get('https://trello-server-xp.herokuapp.com/columns')
       .toPromise()
       .then((columns: any) => columns)
   }
 
   public addColumns(data):Promise<any>{
-    return this.http.post('https://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/columns', data)
+    return this.http.post('https://trello-server-xp.herokuapp.com/columns', data)
       .toPromise()
       .then((columns: any)=> columns)
   }
 
   // Tasks
   public addTask(data, id):Promise<any>{
-    return this.http.put(`https://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/columns/${id}`, data)
+    return this.http.put(`https://trello-server-xp.herokuapp.com/columns/${id}`, data)
       .toPromise()
       .then((cards: any)=> cards)
   }
 
   // Tags
   public getTags(){
-    return this.http.get('https://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/tags')
+    return this.http.get('https://trello-server-xp.herokuapp.com/tags')
     .toPromise()
     .then((tags: any) => tags)
   }
 
   // People
   public getPeople(){
-    return this.http.get('https://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/people')
+    return this.http.get('https://trello-server-xp.herokuapp.com/people')
       .toPromise()
       .then((people:any) => people)
   }
 
   public addUser(data){
-    return this.http.post('https://my-json-server.typicode.com/jonathanslima/trello-clone-fake-api/people', data)
+    return this.http.post('https://trello-server-xp.herokuapp.com/people', data)
       .toPromise()
       .then((user:any)=> user)
   }
