4758f06f4b44a3479c705f7668ff18327b942d51
Lima <jonathan.da.s.lima@accenture.com>
2020-05-21 23:45:01 -0300

    improving layout, changing colors

--- src/app/servicos/data.service.ts
+++ src/app/servicos/data.service.ts
@@ -9,53 +9,55 @@ export class retrieveData{
 
   // Title
   public getTitle():Promise<any>{
-    return this.http.get('https://trello-server-xp.herokuapp.com/titleBoard')
+    return this.http.get('http://localhost:3000/titleBoard')
       .toPromise()
       .then((title: string) => title)
   }
 
   public updateTitle(data):Promise<any>{
-    return this.http.put('https://trello-server-xp.herokuapp.com/titleBoard', data)
+    return this.http.put('http://localhost:3000/titleBoard', data)
       .toPromise()
       .then((res: any) => res)
   }
 
+  // https://trello-server-xp.herokuapp.com/
+  //
   // Columns
   public getColumns():Promise<any>{
-    return this.http.get('https://trello-server-xp.herokuapp.com/columns')
+    return this.http.get('http://localhost:3000/columns')
       .toPromise()
       .then((columns: any) => columns)
   }
 
   public addColumns(data):Promise<any>{
-    return this.http.post('https://trello-server-xp.herokuapp.com/columns', data)
+    return this.http.post('http://localhost:3000/columns', data)
       .toPromise()
       .then((columns: any)=> columns)
   }
 
   // Tasks
   public addTask(data, id):Promise<any>{
-    return this.http.put(`https://trello-server-xp.herokuapp.com/columns/${id}`, data)
+    return this.http.put(`http://localhost:3000/columns/${id}`, data)
       .toPromise()
       .then((cards: any)=> cards)
   }
 
   // Tags
   public getTags(){
-    return this.http.get('https://trello-server-xp.herokuapp.com/tags')
+    return this.http.get('http://localhost:3000/tags')
     .toPromise()
     .then((tags: any) => tags)
   }
 
   // People
   public getPeople(){
-    return this.http.get('https://trello-server-xp.herokuapp.com/people')
+    return this.http.get('http://localhost:3000/people')
       .toPromise()
       .then((people:any) => people)
   }
 
   public addUser(data){
-    return this.http.post('https://trello-server-xp.herokuapp.com/people', data)
+    return this.http.post('http://localhost:3000/people', data)
       .toPromise()
       .then((user:any)=> user)
   }
